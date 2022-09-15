0a7e7bae3891313bbad7e493e8cae3fdbccfa3f7
Axel Brune <prestataire.0165962@medialex.fr>
2022-09-15 16:45:44 +0200

    Suppression HEAD

--- build/components/icons/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IIconProps {
-    color?: string;
-    size?: string | number | (string & {}) | undefined;
-    reverse?: boolean;
-}
-export declare const PrevDocIcon: (props: IIconProps) => any;
-export declare const NextDocIcon: (props: IIconProps) => any;
-export declare const LoadingIcon: (props: IIconProps) => any;
--- build/state/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { FC } from "react";
-import { DocViewerProps } from "..";
-declare const DocViewerContext: any;
-declare const AppProvider: FC<DocViewerProps>;
-export { DocViewerContext, AppProvider };
